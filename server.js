060f5731c43ed06fbb20ba3ccf29643dd9a846df
awee01 <85651950+awee01@users.noreply.github.com>
2021-09-19 19:07:45 -0400

    Added Tag routes

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
+sequelize.sync({force: true}).then(() => {
+  app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
+});
+
 });
