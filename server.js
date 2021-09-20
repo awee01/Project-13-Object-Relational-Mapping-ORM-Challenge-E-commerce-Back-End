6b52a8611e8e7da066f98b3d8bb867e213c676ba
awee01 <85651950+awee01@users.noreply.github.com>
2021-09-20 18:48:50 -0400

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,12 @@ app.use(routes);
 // sequelize.sync({force: true}).then(() => { 
 // Above code was causing error so I removed it, redundant to application use.
 
+sequelize.sync({force: false}).then(() => { 
+
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
+
+
+});
+
